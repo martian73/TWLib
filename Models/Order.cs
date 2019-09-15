666a00245fb7354a1a6c43ba93767f020bdf912c
Marvin White <marvin.white@nike.com>
2019-09-15 03:38:08 -0700

    Order support and more models added.

--- Models/Order.cs
+++ Models/Order.cs
@@ -3,17 +3,91 @@
 
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 
 namespace TWLib.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum OrderType
+    {
+        [EnumMember(Value = "Market")]
+        MARKET,
+        [EnumMember(Value = "Limit")]
+        LIMIT,
+        [EnumMember(Value = "Stop")]
+        STOPMARKET,
+        [EnumMember(Value = "Stop Limit")]
+        STOPLIMIT
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TimeInForce
+    {
+        [EnumMember(Value = "Day")]
+        DAY,
+        [EnumMember(Value = "GTC")]
+        GTC,
+        [EnumMember(Value = "GTD")]
+        GTD,
+        [EnumMember(Value = "Ext")]
+        EXT
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Action
+    {
+        [EnumMember(Value = "Buy to Open")]
+        BUYTOOPEN,
+        [EnumMember(Value = "Buy to Close")]
+        BUYTOCLOSE,
+        [EnumMember(Value = "Sell to Close")]
+        SELLTOCLOSE,
+        [EnumMember(Value = "Sell to Open")]
+        SELLTOOPEN
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum PriceEffect
+    {
+        [EnumMember(Value = "Debit")]
+        DEBIT,
+        [EnumMember(Value = "Credit")]
+        CREDIT
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum InstrumentType
+    {
+        [EnumMember(Value = "Equity")]
+        EQUITY,
+        [EnumMember(Value = "Future")]
+        FUTURE
+    }
+    public class OrderMarket : Order
+    {
+
+    }
+
+    public class LimitOrder : Order
+    {
+
+    }
+
+    public class StopLimit : Order
+    {
+        [JsonProperty("stop-trigger")]
+        public string StopTrigger { get; set; }
+    }
+
     public class Order
     {
         public class Leg
         {
             [JsonProperty("instrument-type")]
-            public string InstrumentType { get; set; }
+            public InstrumentType InstrumentType { get; set; }
 
             [JsonProperty("symbol")]
             public string Symbol { get; set; }
@@ -22,20 +96,22 @@ namespace TWLib.Models
             public double Quantity { get; set; }
 
             [JsonProperty("action")]
-            public string Action { get; set; }
+            public Action Action { get; set; }
         }
 
         [JsonProperty("order-type")]
-        public string OrderType { get; set; }
+        public OrderType OrderType { get; set; }
 
         [JsonProperty("time-in-force")]
-        public string TimeInForce { get; set; }
+        public TimeInForce TimeInForce { get; set; }
 
         [JsonProperty("price")]
         public string Price { get; set; }
 
         [JsonProperty("price-effect")]
-        public string PriceEffect { get; set; }
+        public PriceEffect PriceEffect { get; set; }
+
+
 
         [JsonProperty("legs")]
         public IList<Leg> Legs { get; set; }
--- TWClient.cs
+++ TWClient.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
-using System.Text;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using TWLib.Models;
+using System.Threading;
 
 namespace TWLib
 {
@@ -19,6 +15,7 @@ namespace TWLib
         string Password;
         string APIurl;
         string TokenString;
+
         DateTime LoggedInAt;
         DateTime LastValidate;
 
@@ -26,13 +23,18 @@ namespace TWLib
         bool LoggedIn;
         string UserAgent = "okhttp/3.11.0";
 
+
+        Thread KeepAliveThread;
+
         public TWClient()
         {
+            KeepAliveThread = null;
             LoggedIn = false;
             Cookies = new CookieContainer();
             TokenString = null;
             LoggedInAt = DateTime.MinValue;
             LastValidate = DateTime.MinValue;
+            System.Net.ServicePointManager.Expect100Continue = false;
         }
 
         public bool Init(string username, string password, string apiurl = "https://api.tastyworks.com")
@@ -51,6 +53,8 @@ namespace TWLib
             {
 
             }
+
+            // Start keepalive thread
             return true;
         }
 
@@ -67,24 +71,51 @@ namespace TWLib
             try
             {
                 result = PostJsonRequest("/sessions", @"{""login"": """ + Username + @""", ""password"": """ + Password + @""" }", out response);
+
+                if (response.Cookies != null)
+                    Cookies.Add(response.Cookies);
             }
             catch
             {
                 return null;
             }
 
+            Session session = new Session();
+
+
+            if (response.StatusCode != HttpStatusCode.Created)
+            {
+                LoggedIn = false;
+                LoggedInAt = DateTime.MinValue;
+                LastValidate = DateTime.MinValue;
+                TokenString = null;
+
+                JObject obj = new JObject();
+                string message = (string)obj.SelectToken("error.message");
+                throw new Exception(message);
+            }
+
             if (result != null)
             {
+                LoggedIn = true;
+                JsonSerializer serializer = new JsonSerializer();
+
+                session = JsonConvert.DeserializeObject<Session>(result);
                 JObject ob = JObject.Parse(result);
 
-                string token = (string)ob.SelectToken("data.session-token");
-                LoggedIn = true;
-                LoggedInAt = DateTime.UtcNow;
+                string token = session.Data.SessionToken;
 
                 return token;
             }
 
             return null;
+        }
+
+        private StreamerTokens GetStreamerToken()
+        {
+            StreamerTokens tokens = new StreamerTokens();
+
+            return tokens;
         }
 
         public void ValidateSession()
@@ -113,12 +144,11 @@ namespace TWLib
                 string message = (string)obj.SelectToken("error.message");
                 throw new Exception(message);
             }
-
+            
             LastValidate = DateTime.UtcNow;
             return;
         }
-
-
+        
         public Accounts GetAccounts()
         {
             ValidateSession();
@@ -130,7 +160,6 @@ namespace TWLib
 
             HttpWebResponse wresponse;
             WebHeaderCollection headers = new WebHeaderCollection();
-
             headers.Add("Authorization", TokenString);
 
             string response = GetJsonRequest("/customers/me/accounts", out wresponse, headers);
@@ -158,6 +187,7 @@ namespace TWLib
             //}
             return accounts;
         }
+
         public Balances Balances(string accountName)
         {
             ValidateSession();
@@ -197,12 +227,238 @@ namespace TWLib
             return balances;
         }
 
+        public OrdersLive OrdersLive(string accountName)
+        {
+            ValidateSession();
+
+            if (LoggedIn == false)
+                throw new Exception("Not logged in");
+
+            OrdersLive OrdersLive = new OrdersLive();
+
+            HttpWebResponse wresponse;
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", TokenString);
+
+            string response = GetJsonRequest("/accounts/" + accountName + "/orders/live", out wresponse, headers);
+
+            if (wresponse.StatusCode != HttpStatusCode.OK)
+            {
+                LoggedIn = false;
+                LoggedInAt = DateTime.MinValue;
+                LastValidate = DateTime.MinValue;
+                TokenString = null;
+
+                JObject obj = new JObject();
+                string message = (string)obj.SelectToken("error.message");
+                throw new Exception(message);
+            }
+
+            JsonSerializer serializer = new JsonSerializer();
+            //try
+            //{
+            OrdersLive = JsonConvert.DeserializeObject<OrdersLive>(response);
+            //}
+            //catch (Exception ex)
+            //{
+            //  
+            //}
+            return OrdersLive;
+        }
+
+        public TradingStatus TradingStatus(string accountName)
+        {
+            ValidateSession();
+
+            if (LoggedIn == false)
+                throw new Exception("Not logged in");
+
+            TradingStatus TradingStatus = new TradingStatus();
+
+            HttpWebResponse wresponse;
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", TokenString);
+
+            string response = GetJsonRequest("/accounts/" + accountName + "/trading-status", out wresponse, headers);
+
+            if (wresponse.StatusCode != HttpStatusCode.OK)
+            {
+                LoggedIn = false;
+                LoggedInAt = DateTime.MinValue;
+                LastValidate = DateTime.MinValue;
+                TokenString = null;
+
+                JObject obj = new JObject();
+                string message = (string)obj.SelectToken("error.message");
+                throw new Exception(message);
+            }
+
+            JsonSerializer serializer = new JsonSerializer();
+            //try
+            //{
+            TradingStatus = JsonConvert.DeserializeObject<TradingStatus>(response);
+            //}
+            //catch (Exception ex)
+            //{
+            //  
+            //}
+            return TradingStatus;
+        }
+
+        public MarginReport MarginReport(string accountName)
+        {
+            ValidateSession();
+
+            if (LoggedIn == false)
+                throw new Exception("Not logged in");
+
+            MarginReport MarginReport = new MarginReport();
+
+            HttpWebResponse wresponse;
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", TokenString);
+
+            string response = GetJsonRequest("/margin/accounts/" + accountName + "/report", out wresponse, headers);
+
+            if (wresponse.StatusCode != HttpStatusCode.OK)
+            {
+                LoggedIn = false;
+                LoggedInAt = DateTime.MinValue;
+                LastValidate = DateTime.MinValue;
+                TokenString = null;
+
+                JObject obj = new JObject();
+                string message = (string)obj.SelectToken("error.message");
+                throw new Exception(message);
+            }
+
+            JsonSerializer serializer = new JsonSerializer();
+            //try
+            //{
+            MarginReport = JsonConvert.DeserializeObject<MarginReport>(response);
+            //}
+            //catch (Exception ex)
+            //{
+            //  
+            //}
+            return MarginReport;
+        }
+
+        public Positions Positions(string accountName, bool include_closed_positions=false)
+        {
+            ValidateSession();
+
+            if (LoggedIn == false)
+                throw new Exception("Not logged in");
+
+            Positions positions = new Positions();
+
+            HttpWebResponse wresponse;
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", TokenString);
+
+            string uri = String.Format("/accounts/{0}/positions?include_closed_positions={1}", accountName, include_closed_positions.ToString()); 
+            string response = GetJsonRequest(uri, out wresponse, headers);
+
+            if (wresponse.StatusCode != HttpStatusCode.OK)
+            {
+                LoggedIn = false;
+                LoggedInAt = DateTime.MinValue;
+                LastValidate = DateTime.MinValue;
+                TokenString = null;
+
+                JObject obj = new JObject();
+                string message = (string)obj.SelectToken("error.message");
+                throw new Exception(message);
+            }
+
+            JsonSerializer serializer = new JsonSerializer();
+            //try
+            //{
+            positions = JsonConvert.DeserializeObject<Positions>(response);
+            //}
+            //catch (Exception ex)
+            //{
+            //  
+            //}
+            return positions;
+        }
+
+        public MarketMetrics MarketMetrics(string symbol)
+        {
+            ValidateSession();
+
+            if (LoggedIn == false)
+                throw new Exception("Not logged in");
+
+            MarketMetrics metrics = new MarketMetrics();
+
+            HttpWebResponse wresponse;
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", TokenString);
+
+            string uri = String.Format("/market-metrics?symbols={0}", symbol);
+            string response = GetJsonRequest(uri, out wresponse, headers);
+
+            if (wresponse.StatusCode != HttpStatusCode.OK)
+            {
+                LoggedIn = false;
+                LoggedInAt = DateTime.MinValue;
+                LastValidate = DateTime.MinValue;
+                TokenString = null;
+
+                JObject obj = new JObject();
+                string message = (string)obj.SelectToken("error.message");
+                throw new Exception(message);
+            }
+            
+            //try
+            //{
+            metrics = JsonConvert.DeserializeObject<MarketMetrics>(response);
+            //}
+            //catch (Exception ex)
+            //{
+            //  
+            //}
+            return metrics;
+        }
+
+        public Orders ExecuteOrder(string accountID, Order order)
+        {
+            Orders orders = new Orders();
+
+            ValidateSession();
+
+            if (LoggedIn == false)
+                throw new Exception("Not logged in");
+
+            MarketMetrics metrics = new MarketMetrics();
+
+            HttpWebResponse wresponse;
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", TokenString);
+            headers.Add("Accept-Encoding", "gzip, deflate");
+            string uri = String.Format("/accounts/{0}/orders", accountID);
+            string payload = JsonConvert.SerializeObject(order);
+            string response = PostJsonRequest(uri, payload, out wresponse, headers);
+           
+            if (wresponse.StatusCode == HttpStatusCode.OK)
+            {
+                orders = JsonConvert.DeserializeObject<Orders>(response);
+                return orders;
+            }
+
+            Orders err = new Orders();
+            err = JsonConvert.DeserializeObject<Orders>(response);
+            return err;
+        }
+
         public string GetJsonRequest(string path, out HttpWebResponse response, WebHeaderCollection headers = null)
         {
             // create a request
             HttpWebRequest request = (HttpWebRequest)
             WebRequest.Create(APIurl + path); request.KeepAlive = false;
-            request.ProtocolVersion = HttpVersion.Version10;
+            request.ProtocolVersion = HttpVersion.Version11;
             request.Method = "GET";
 
             // this is important - make sure you specify type this way
@@ -217,9 +473,28 @@ namespace TWLib
             request.UserAgent = UserAgent;
 
             // grab te response and print it out to the console along with the status code
-            response = (HttpWebResponse)request.GetResponse();
 
             string result;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                response = (HttpWebResponse)ex.Response;
+                using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
+                {
+                    result = rdr.ReadToEnd();
+                    return result;
+                }
+            }
+
+            if (response != null && response.Cookies != null)
+            {
+                Cookies.Add(response.Cookies);
+            }
+            
             using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
             {
                 result = rdr.ReadToEnd();
@@ -229,13 +504,20 @@ namespace TWLib
         }
 
         public string PostJsonRequest(string path, string json, out HttpWebResponse response, WebHeaderCollection headers = null)
-        { 
+        {
             // create a request
-            HttpWebRequest request = (HttpWebRequest)
-            WebRequest.Create(APIurl + path); request.KeepAlive = false;
-            request.ProtocolVersion = HttpVersion.Version10;
-            request.Method = "POST";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(APIurl + path);
+            
+            if (headers != null)
+            {
+                headers.Add("Accept-Version", "v1");
+                request.Headers = headers;
+            }
 
+            request.UserAgent = UserAgent;
+            request.KeepAlive = false;
+            request.Method = "POST";
+            request.ProtocolVersion = HttpVersion.Version11;
 
             // turn our request string into a byte stream
             byte[] postBytes = null;
@@ -245,16 +527,11 @@ namespace TWLib
             }
 
             // this is important - make sure you specify type this way
-            request.ContentType = "application/json; charset=UTF-8";
+            request.ContentType = "application/json; charset=utf-8";
             request.Accept = "application/json, text/javascript";
-            request.ContentLength = (postBytes != null ) ? postBytes.Length : 0;
+            request.ContentLength = (postBytes != null) ? postBytes.Length : 0;
             request.CookieContainer = Cookies;
-            if (headers != null)
-            {
-                headers.Add("Accept-Version", "v1");
-                request.Headers = headers;
-            }
-            request.UserAgent = UserAgent;
+
 
             Stream requestStream = request.GetRequestStream();
 
@@ -265,15 +542,31 @@ namespace TWLib
             }
             requestStream.Close();
 
-            // grab te response and print it out to the console along with the status code
-            response = (HttpWebResponse)request.GetResponse();
-            
             string result;
+
+            // grab te response and print it out to the console along with the status code
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                if (response != null && response.Cookies != null)
+                {
+                    Cookies.Add(response.Cookies);
+                }
+            }
+            catch (WebException ex)
+            {
+                response = (HttpWebResponse)ex.Response;
+                using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
+                {
+                    result = rdr.ReadToEnd();
+                    return result;
+                }
+            }
+
             using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
             {
                 result = rdr.ReadToEnd();
             }
-
             return result;
         }
     }
